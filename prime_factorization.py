ca414d21e039f89100b710e62b7bb02fec40670b
Joseph Pickert <pickertjoe@gmail.com>
2019-05-29 09:23:49 -0600

    Adding .gitignore

---
+++ prime_factorization.py
@@ -0,0 +1,8 @@
+# A simple program to return all prime factors of a user-provided whole number
+
+
+def main():
+    """A function to serve as the main menu and call children functions as necessary"""
+    while True:
+        print("~~~Welcome to the Python Prime Factor Machine!~~~")
+        print("When given a value, I can show you all the prime factors of that number.")
