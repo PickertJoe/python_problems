085ec791a0251a072e5a39849d99a55be72d21b6
Joseph Pickert <pickertjoe@gmail.com>
2019-06-02 10:16:40 -0500

    Refactoring and trying to solve square number issue

--- prime_factorization.py
+++ prime_factorization.py
@@ -28,28 +28,59 @@ def main():
             print("Number out of range. Returning to main...")
             continue
 
-        prime_factors = []
-        if test_number % 2 == 0 and test_number != 2:
-            prime_factors.append(2)
-        for number_1 in range(3, test_number):
-            if test_number % number_1 == 0:
-                for number_2 in range(2, number_1):
-                    if number_1 % number_2 == 0:
-                        break
-                    elif number_1 in prime_factors:
-                        break
-                    else:
-                        prime_factors.append(number_1)
-        show_results(test_number, prime_factors)
+        factor_array = []
+        prime_array = []
+
+        # Calling get_factors function to fill the factors array
+        factor_array = get_factors(test_number, factor_array)
+
+        # Calling get_prime function to find all the prime factors
+        prime_array = get_prime(test_number, factor_array, prime_array)
+
+#        for number_1 in range(3, test_number):
+ #           if test_number % number_1 == 0:
+  #              for number_2 in range(2, number_1):
+   #                 if number_1 % number_2 == 0:
+    #                    break
+     #               elif number_1 in prime_factors:
+      #                  break
+       #             else:
+        #                prime_factors.append(number_1)
+        show_results(test_number, factor_array, prime_array)
 
 
-def show_results(user_input, array):
+def get_factors(user_input, array):
+    """A function to find all possible factors for the given number"""
+    for number in range(2, user_input):
+        if user_input % number == 0:
+            array.append(number)
+    return array
+
+
+def get_prime(user_input, factor_array, prime_array):
+    """A function to sort through factor array to find all primes"""
+    for factor in factor_array:
+        testing = True
+        while testing:
+            for number in range(2, factor):
+                if factor % number == 0:
+                    testing = False
+                    break
+        prime_array.append(factor)
+    return prime_array
+
+
+def show_results(user_input, factor_array, prime_array):
     """Simple function to show the final results of the program"""
-    if len(array) > 1:
-        print("The prime factors of " + str(user_input) + " are:")
-        print(array)
+    if factor_array:
+        print("The factors of " + str(user_input) + " are: " + str(factor_array))
+        if not prime_array:
+            print("The number " + str(user_input) + " has not prime factors!")
+        else:
+            print("The prime factors of " + str(user_input) + " are:")
+            print(prime_array)
     else:
-        print(str(user_input) + " is either already a prime number number or has no direct prime factors!")
+        print("The number " + str(user_input) + " is already prime!")
 
 
 main()
