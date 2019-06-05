d698d0a90f4e6690e22b5a0965e463adb7db0fed
Joseph Pickert <pickertjoe@gmail.com>
2019-06-05 13:30:55 -0600

    Tiling cost calculator

--- tiling_cost_calculator.py
+++ tiling_cost_calculator.py
@@ -3,16 +3,41 @@
 
 def main():
     """A function to serve as the main menu"""
-    while True:
-        print("\n~~~Welcome to the Python Tiling Cost Estimator~~~")
-        print("For a given floorspace, I can estimate the cost to tile it to the nearest $.01")
-        print("To exit at any time, simply type 'Q'.")
-        width = input("Please enter the width of your floorspace in meters: ")
-        if width.title() == "Q":
-            break
-        height = input("Please enter the height of your floorspace in meters: ")
-        if height.title() == "Q":
-            break
-        opt_pricing = ("Tile here cost $50/m2. Would you like to enter a different price(Y/N)? ")
-        if opt_pricing.title() == "Y":
-            new_price = input("Please enter the new price you'd like to use: ")
+    print("~~~Welcome to the Python Tiling Cost Estimator~~~")
+    print("I can help you estimate the cost to tile your new home to the nearest $.01")
+    print("Simply enter the floorspace dimensions and tiling cost for each room.")
+    print("And I'll calculate the total cost.")
+    start_prompt = input("Shall we begin?(Y/N): ")
+
+    if start_prompt.title() == "N":
+        print("So long!")
+
+    else:
+        roomcosts = []
+        while True:
+            roomcosts = room_calculate(roomcosts)
+            continue_prompt = input("Would you like to add another room?(Y/N): ")
+            if continue_prompt.title() == "N":
+                show_results(roomcosts)
+                break
+            else:
+                continue
+
+
+def room_calculate(array):
+    """A function to calculate the flooring cost for each room"""
+    width = input("Please enter the width of your floorspace in meters: ")
+    height = input("Please enter the height of your floorspace in meters: ")
+    price = input("Please enter the price of your tiling: ")
+    cost = float(width) * float(height) * float(price)
+    array.append(cost)
+    return array
+
+
+def show_results(array):
+    """A function to show the final results of the calculation"""
+    final = sum(array)
+    print("The grand total to tile your home is: $" + str(round(final, 2)))
+
+
+main()
